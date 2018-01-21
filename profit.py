26246286388bc7e962a23859c29e2a3083190b69
Jean Ruffato <jean.ruffato@gmail.com>
2018-01-21 12:40:49 -0200

    Commit inicial

---
+++ profit.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Responsável: Jean Ruffato
+    Data: 21/01/2018
+    Descrição: Script python para calcular lucro de um investimento no mercado de ações
+"""
+
+
+print('Ticker', '        Quantidade', '       Valor', '     Varição %')
