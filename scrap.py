dc5c9a51b208b96e55e2dc957d1e46638b777d23
Jean Ruffato <jean.ruffato@gmail.com>
2018-02-28 21:22:10 -0300

    Adicionado funcionalidade para análise de codigo HTML

--- scrap.py
+++ scrap.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-    Descrição: Script para obter informações de análise fundamentalista
-    registradas no site http://www.fundamentus.com.br/ e inserir em um
-    banco de dados para consulta dos scripts e execução dos calclos
+    Description: Script to obtain fundamental analysis information registered
+    in the site http://www.fundamentus.com.br/ and to insert a database for
+    consultation of the scripts and execution of the calculations.
 """
 
 __author__ = "Jean Ruffato"
@@ -14,13 +14,29 @@ __date__ = "2018/02/27"
 
 
 import requests
-# from bs4 import BeautifulSoup
+from html.parser import HTMLParser
 
 
 def ambev():
-    # Função para realizar scrapping das informações das ações da Ambev
-    response = requests.get('http://201.82.5.94:8080')
+    # Function to perform scraping of Ambev's stock information
+    response = requests.get('http://www.fundamentus.com.br/detalhes.php?papel=ABEV3&x=48&y=9')
+    analyzer_base = str(response.content)
+
+    class Response_analyzer(HTMLParser):
+        # Function to generate elegant error messages
+        def error(self, message):
+            pass
+
+        # Function to extract data from Abev's information page
+        def handle_data(self, data):
+            print("Data:", data, HTMLParser.getpos(self))
+
+    # Code that pass html content to Response analyzer execute your job
+    Response_analyzer().feed(analyzer_base)
 
 
 def bancobrasil():
     pass
+
+
+print(ambev())
